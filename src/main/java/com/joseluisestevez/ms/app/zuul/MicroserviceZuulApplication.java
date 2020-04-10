b32b7ef8a5417de9b9f3e868a3abf5972e108aa7
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-10 14:06:08 +0200

    New Spring Boot project - Zuul Server

---
+++ src/main/java/com/joseluisestevez/ms/app/zuul/MicroserviceZuulApplication.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.ms.app.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceZuulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joseluisestevez/ms/app/zuul/MicroserviceZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.ms.app.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
